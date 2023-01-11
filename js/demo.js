2c1cbfe0d0f392b56a86ec4f8a140f672aaf4fac
cmndcntrlcyber <you@example.com>
2023-01-10 19:20:13 -0800

    added mirrors

---
+++ js/demo.js
@@ -0,0 +1,28 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#000000',
+    lineColor: '#000000'
+  });
+  var main = document.getElementById('main');
+  main.style.marginTop = - main.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.main').css({
+    'margin-top': -($('.main').height() / 2)
+  });
+});
+*/
