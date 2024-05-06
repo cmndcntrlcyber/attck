d88bc3a4de0c21e2d778090deaee9be4ae55b13f
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-05-06 01:51:35 -0400

    updated files

---
+++ attcks/payloads/rust/elevator/target/release/build/winapi-x86_64-pc-windows-gnu-dc390982022fd0e7/build_script_build-dc390982022fd0e7.d
@@ -0,0 +1,5 @@
+/home/cmndcntrl/code/attck/attcks/payloads/rust/elevator/target/release/build/winapi-x86_64-pc-windows-gnu-dc390982022fd0e7/build_script_build-dc390982022fd0e7: /home/cmndcntrl/.cargo/registry/src/github.com-1ecc6299db9ec823/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs
+
+/home/cmndcntrl/code/attck/attcks/payloads/rust/elevator/target/release/build/winapi-x86_64-pc-windows-gnu-dc390982022fd0e7/build_script_build-dc390982022fd0e7.d: /home/cmndcntrl/.cargo/registry/src/github.com-1ecc6299db9ec823/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs
+
+/home/cmndcntrl/.cargo/registry/src/github.com-1ecc6299db9ec823/winapi-x86_64-pc-windows-gnu-0.4.0/build.rs:
